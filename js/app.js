b932c956855f683de2178bb050f73548c2ca93a8
flpego <flaviopegoraro2@gmail.com>
2023-11-15 12:49:57 -0300

    terminamos estilos, aniadimos funcionalidad para poder clckear en los sinonimos, queda manejar la llamada a la api y renderizar el resultado

--- js/app.js
+++ js/app.js
@@ -13,43 +13,35 @@ async function apiRequest() {
         console.log(data)
 
         if (data.title === 'No Definitions Found') {
-
+            //
             result.innerHTML = `<h4>${data.title}</h4>`
 
         } else {
             // Mostrar resultados normales si se encontraron definiciones
-            result.innerHTML = `
-            <h4>${inputWord}</h4>
-            <div class="word-header-container">
-            <p>Fonética: ${data[0].phonetic}</p>
-            <button onclick="playSound()">Sound</button>
-            </div>
-    
-      
-            <div class="definitions-container">
-            <h4>definitions</h4>
-              <ul>
-                ${data[0].meanings[0].definitions.map(definition => `
-                <li>${definition.definition}</li>
-                `).join("")}
-              </ul>
-            </div>
-
-            <div class="synonym-container">    
-            <h4>synonyms</h4>
-            <ul>
-            ${data[0].meanings[0].synonyms.map(synonym => `
-            <li>${synonym}</li>
-            `).join('')}
-          </ul>
-            </div>            
-      
-           
-            <p></p>
-            `;
+            result.innerHTML = renderResults(inputWord, data);
 
             sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
             console.log(sound);
+
+            const defListContainer = document.querySelector(".definitions-container");
+            let defHTML = "";
+
+            for (let i = 0; i < data[0].meanings.length; i++) {
+                defHTML += `
+                <div class="${data[0].meanings[i].partOfSpeech} def-grid">
+                    <h4>${data[0].meanings[i].partOfSpeech}</h4>
+                    <ul>
+                    ${data[0].meanings[i].definitions.map(definition =>
+                    `<li>${definition.definition}</li>`
+                ).join("")}
+                    </ul>
+                </div>
+                `
+            }
+
+            defListContainer.innerHTML = defHTML;
+
+
         }
 
     } catch (error) {
@@ -57,15 +49,59 @@ async function apiRequest() {
         result.innerHTML = `<h4>404</h4>`;
     }
 
-}
+};
+
+function renderResults(inputWord, data) {
+    return ` <div class="word-header-container">
+    <h4>${inputWord}</h4>
+  <p>Fonética: <span>${data[0].phonetic}</span> </p>
+  <button class="play-button" onclick="playSound()">Listen<i class="fa-solid fa-volume-high"></i></button>
+</div>
+
+<div class="definitions-container">
+</div>
+
+<div class="synonym-container">
+  <h4>Synonyms</h4>
+  <ul>
+    ${data[0].meanings[0].synonyms.length > 0
+            ? data[0].meanings[0].synonyms.map(synonym => `
+        <li>
+        <a class="synonym-link" href="#">${synonym}</a>
+      </li>`).join('')
+            : '<li>No synonyms found</li>'}
+  </ul>
+</div>`
+};
+
+
 
 function playSound() {
     sound.play();
-}
+};
+
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("synonym-link")) {
+        event.preventDefault();
+
+        const inputWord = event.target.textContent;
+
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                //proximamente sera implementado el manejo a la nueva solicitud
+            })
+            .catch(error => {
+                console.error("Error en la solicitud a la API:", error.message);
+            });
+    }
+});
+
 
 searchButton.addEventListener("click", function (e) {
     e.preventDefault();
     apiRequest();
-})
+});
 
 
